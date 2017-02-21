c800de0c8a535c34f4a4bc29e4104b4b210393c5
Jenna <jenna.galka@gmail.com>
2017-02-21 09:32:46 -0500

    Basically the whole game
    Minus some scenes and tweaking, the core is here

---
+++ New Unity Project/Assets/scripts/HealthManagerScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManagerScript : MonoBehaviour {
+
+	// VARIABLES FOR HEALTH MAINTENANCE
+	private const int BOX_HEALTH_MIN = 0;
+	public const int BOX_HEALTH_MAX = 50;
+
+	private static int boxHealth;
+
+	// BoxHealth property
+	public int BoxHealth{
+		get {
+			return boxHealth;
+		}
+
+		set {
+			boxHealth = value;
+
+			if (boxHealth > BOX_HEALTH_MAX) {
+				boxHealth = BOX_HEALTH_MAX;
+			}
+
+			if (boxHealth < BOX_HEALTH_MIN) {
+				boxHealth = BOX_HEALTH_MIN;
+			}
+		}
+	}
+
+	// VARIABLES FOR SCORING AND SAVING
+	private const string WINNER_STREAK = "winnerStreak";
+
+	private int streak;
+
+	// Streak property
+	public int Streak{
+		get {
+			return streak;
+		}
+
+		set{
+			streak = value;
+
+			if (streak > StreakValue) {
+				StreakValue = streak;
+			}
+
+			Debug.Log (streak);
+		}
+	}
+
+	private int streakValue = 3;
+
+	// StreakValue property
+	public int StreakValue{
+		get { 
+			streakValue = PlayerPrefs.GetInt (WINNER_STREAK);
+			return streakValue;
+		}
+
+		set {
+			Debug.Log ("New Streak!!!");
+			streakValue = value;
+			PlayerPrefs.SetInt (WINNER_STREAK, streakValue);
+		}
+	}
+
+	public static HealthManagerScript instance;
+
+	// Use this for initialization
+	void Start () {
+
+		// don't destroy on load
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (this);
+		} else {
+			Destroy (gameObject);
+		}
+
+		// keep the health the max score
+		BoxHealth = BOX_HEALTH_MAX;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (boxHealth);
+		Debug.Log (streakValue);
+
+//		// HERE, WE NEED TO KEEP TRACK OF SCORES
+//		// AS WELL AS KEEP TRACK OF WHO HAS TAKEN AWAY MORE HEALTH
+//		// WHOEVER HAS TAKEN AWAY MORE HEALTH IS WINNER
+//		// RELOAD LEVEL
+//		// IF THEY HAVE WON MORE THAN THE STREAK NUMBER, YAY, ALSO KEEP GOING
+//		// IF NOT, KEEP PLAYING
+//		if (boxHealth >= BOX_HEALTH_MIN) {
+//			if 
+//		}
+		
+	}
+}
---
+++ New Unity Project/Assets/scripts/ScorerScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorerScript : MonoBehaviour {
+
+	// GAME OBJECTS
+	public GameObject player1, player2;
+
+	// VARIABLES FOR DAMAGE AND SPEED
+	public int player1Damage;
+	public int player2Damage;
+	public PlayerMovementScript pms;
+	private const int MIN_SPEED = 1;
+	private const int MAX_SPEED = 50;
+
+	// FIND THE HEALTH STUFF
+	public GameObject gameManager;
+	public HealthManagerScript hms;
+
+	// Use this for initialization
+	void Start () {
+
+		gameManager = GameObject.Find("GameManager");
+		hms = gameManager.GetComponent<HealthManagerScript> ();
+		pms = gameObject.GetComponent<PlayerMovementScript> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (this.pms.speed < MIN_SPEED) {
+			this.pms.speed = MIN_SPEED;
+		}
+
+		if (this.pms.speed > MAX_SPEED) {
+			this.pms.speed = MAX_SPEED;
+		}
+
+		//if (gameObject.name.Contains(2) )
+		
+	}
+
+	// I was going to try to fix this so that only one could do damage at a time,
+	// but I actually find it kind of compelling that both do damage. Still, that would
+	// change the win condition somewhat, so there has to be a way for both players to
+	// put health back, rather than just one.
+	void OnCollisionEnter2D(Collision2D other) {
+
+		if (other.gameObject.name.Contains("Player1")) {
+			Damager (player2, player2Damage);
+		} else if (other.gameObject.name.Contains("Player2")) {
+			Damager (player1, player1Damage);
+		}
+
+	}
+
+	public void Damager(GameObject go, int damage){
+
+		hms.BoxHealth = hms.BoxHealth - damage;
+
+		if (go == player1) {
+			damage++;
+			pms.speed--;
+			Debug.Log ("Damaging " + damage);
+
+		} else if (go == player2) {
+			damage--;
+			pms.speed++;
+			Debug.Log ("Damaging " + damage);
+		}
+
+		if (damage > 0) {
+			damage = 0;
+		}
+	}
+}
